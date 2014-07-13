2d1fc58c26e3af812b900a645fda9f9536a907a1
bill <billsearle@me.com>
2014-07-14 09:15:51 +1000

    jQuery carousel

--- hackers/bill_searle/wk6d5/carousel/js/carousel.js
+++ hackers/bill_searle/wk6d5/carousel/js/carousel.js
@@ -1,10 +1,63 @@
+// var moveNext = function(){
+// 	if ($('#carousel').css('left') !== $('#carousel').css('width')) {
+// 		$('#carousel').animate({
+// 			left: "-=612px"}, 500)
+// 		} else {
+// 			$('#carousel').animate({
+// 			left: "+=1224px"}, 500)
+// 		}
+// 	};
+
+// var movePrevious = function(){
+// 	if ($('#carousel').css('left') !== "0px") {
+// 	$('#carousel').animate({
+// 		left: "+=612px"}, 500)
+// 	} else {
+// 		$('#carousel').animate({
+// 			left: "-=1224px"}, 500)
+// 	}
+// };
+
+// $('#next').click(moveNext);
+// $('#previous').click(movePrevious);
+
+
+
 var moveNext = function(){
-	$('#carousel').animate({
-		left: "-=612px"}, 500)
-};	
+	if ($('#carousel').css('left') !== "-1224px") {
+		$('#carousel').animate({
+			left: "-=612px"}, 500)
+		} else {
+			$('#carousel').animate({
+			left: "+=1224px"}, 500)
+		}
+	};
+
 var movePrevious = function(){
+	if ($('#carousel').css('left') !== "0px") {
 	$('#carousel').animate({
 		left: "+=612px"}, 500)
+	} else {
+		$('#carousel').animate({
+			left: "-=1224px"}, 500)
+	}
 };
+
 $('#next').click(moveNext);
+$('#next').keypress(moveNext);
+
 $('#previous').click(movePrevious);
+
+
+
+
+// var moveNext = function(){
+// 	$('#carousel').animate({
+// 		left: "-=612px"}, 500)
+// };	
+// var movePrevious = function(){
+// 	$('#carousel').animate({
+// 		left: "+=612px"}, 500)
+// };
+// $('#next').click(moveNext);
+// $('#previous').click(movePrevious);
